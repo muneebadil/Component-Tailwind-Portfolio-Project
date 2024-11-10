5a9cb0118cbfda457f1f576d6f463e845e505bea
Mohammad Muneeb Adil <mohammadmuneeb197@gmail.com>
2024-11-10 13:59:59 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Hero from "./component/hero";
+import Contact from "./component/contact";
+import Project from "./component/project";
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero/>
+      <Project/>
+      <Contact/>
+    </div>
+  );
+}
+
